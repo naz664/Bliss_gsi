a5b4c88790bd23d6582168c7d5c1e72883cade4d
naz664 <nazimnavas145@gmail.com>
2023-01-11 12:57:45 +0000

    Build: Update the script

--- bliss.mk
+++ bliss.mk
@@ -19,3 +19,20 @@ TARGET_BOOT_ANIMATION_RES := 1080
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.system.ota.json_url=https://raw.githubusercontent.com/naz664/Bliss_gsi/13/ota.json
 
+# Use CCache
+USE_CCACHE := true
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+        persist.sys.binary_xml=false \
+        dalvik.vm.dex2oat64.enabled=true \
+
+#prebuilts
+PRODUCT_PACKAGES += \
+  OpenEUICC \
+
+# Set Bootanimation at 720P
+TARGET_BOOT_ANIMATION_RES := 720
+
+# APN
+PRODUCT_PACKAGES += apns-conf.xml
+
